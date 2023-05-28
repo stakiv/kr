e5848814b7faa844a38563b3d047c9ef878c59c2
stakiv <125588447+stakiv@users.noreply.github.com>
2023-05-28 20:12:44 +0300

    Add files via upload

---
+++ container.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+class container {
+private:
+	int num;
+public:
+	void set(int num_) {
+		num = num_;
+	}
+	int quantity() {
+		num -= 1;
+		return (num);
+	}
+};
